ccfc565986854bb314e9bfd04f992e8507455083
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-05 23:42:38 -0300

    feat:setup ngrx and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/ui/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: async () =>
+      (await import('./slices/home/routes/routes')).routes,
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
