d06c9658b213f587a435a1a1d9f5aef6ce3d5a29
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-07 20:40:41 -0300

    feat:calculate facade

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,16 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouterStore, RouterState } from '@ngrx/router-store';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { httpInterceptor } from './core/interceptor/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([httpInterceptor])),
+    provideHttpClient(),
     provideRouterStore({
       stateKey: 'router',
       routerState: RouterState.Full,
--- src/app/slices/signals/data-access/facade-calculator.ts
+++ src/app/slices/signals/data-access/facade-calculator.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { effect, inject, Injectable, signal } from '@angular/core';
 import { OptionCalculator } from '../../../shared/types/interfaces/option-calculator';
 import { CalculatorHttpService } from '../../../core/services/calculator-http-service';
 import { tap } from 'rxjs';
@@ -7,12 +7,14 @@ export type CalculatorState = {
   optionsCalculator: OptionCalculator[];
   result: number;
   expression: string;
+  isValidation: boolean;
 };
 
 export const initialState: CalculatorState = {
   optionsCalculator: [],
   result: 0,
   expression: '',
+  isValidation: true,
 };
 
 @Injectable({ providedIn: 'root' })
@@ -20,6 +22,12 @@ export class FacadeCalculator {
   private state = signal(initialState);
   readonly vm$ = this.state.asReadonly();
   #httpCalculator = inject(CalculatorHttpService);
+
+  constructor() {
+    effect(() => {
+      console.log('effect', this.state());
+    });
+  }
 
   loadOptions() {
     return this.#httpCalculator.getCalculator().pipe(
