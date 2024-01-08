5e203dded0f18891df6088fd005579c9bd4f39dc
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-07 23:45:03 -0300

    feat:calculate function

--- src/app/slices/signals/data-access/facade-calculator.ts
+++ src/app/slices/signals/data-access/facade-calculator.ts
@@ -1,13 +1,17 @@
 import { effect, inject, Injectable, signal } from '@angular/core';
 import { OptionCalculator } from '../../../shared/types/interfaces/option-calculator';
 import { CalculatorHttpService } from '../../../core/services/calculator-http-service';
-import { tap } from 'rxjs';
+import { filter, tap } from 'rxjs';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { CalculatorService } from '../utils/calculator.shutting.hard.service';
 
 export type CalculatorState = {
   optionsCalculator: OptionCalculator[];
   result: number;
   expression: string;
   isValidation: boolean;
+  lastExpression: string;
+  isCalculate:boolean;
 };
 
 export const initialState: CalculatorState = {
@@ -15,18 +19,56 @@ export const initialState: CalculatorState = {
   result: 0,
   expression: '',
   isValidation: true,
+  lastExpression: '',
+  isCalculate:false,
 };
+
 
 @Injectable({ providedIn: 'root' })
 export class FacadeCalculator {
   private state = signal(initialState);
   readonly vm$ = this.state.asReadonly();
   #httpCalculator = inject(CalculatorHttpService);
+  #calculatorService = inject(CalculatorService);
+
+  
+  expresionFinalizated$=toObservable(this.state).pipe(
+    filter(state=>state.expression.length>0 && state.expression[state.expression.length-1]==='='),
+    tap((state) => {
+        const expression=state.expression.slice(0,state.expression.length-1);
+        const result=this.#calculatorService.calculateExpression(expression);
+        this.state.update(state=>({...state,
+          result:result,
+          lastExpression:expression,
+          expression:result.toString(),
+          isValidation:true,
+          isCalculate:true,
+        }));
+    }));
+
+
+
+  expressionClean$=toObservable(this.state).pipe(
+    filter(state=>state.expression.length>0 && state.expression[state.expression.length-1]==='C'),
+    tap(state=>this.state.update(state=>({...state,expression:''}))),
+  );
+
+
+
 
   constructor() {
     effect(() => {
       console.log('effect', this.state());
     });
+
+    this.expresionFinalizated$.subscribe((value) => {
+      console.log('expression$', value);
+    });
+
+    this.expressionClean$.subscribe((value) => {
+      console.log('expressionClean$', value);
+    });
+
   }
 
   loadOptions() {
@@ -44,6 +86,7 @@ export class FacadeCalculator {
     this.state.update(state => ({
       ...state,
       expression: state.expression + option.visibleValue,
+      isCalculate:false,
     }));
   }
 }
